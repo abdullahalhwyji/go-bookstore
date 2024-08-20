a1ae30691bbef9939895bc197e93e491819fb3ba
Abdullah Alhwyji <abdullah.alhwyji@gmail.com>
2024-08-20 16:27:24 +0700

    edit the code with sql connection

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/alhwyji/go-bookstore/pkg/routes"
+	"github.com/gorilla/mux"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+
+func main(){
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRouters(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", `root:@tcp(localhost:3306)/books?charset=utf8&parseTime=True&loc=Local`)
+
+	if err != nil {
+		panic(err)
+	}
+	if err == nil {
+		fmt.Println("The application is running")
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
